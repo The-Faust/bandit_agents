cea59ab592237f18f176a064efa7cf741680addc
The-Faust <vincent.martel.11235@outlook.com>
2025-01-23 16:43:07 -0500

    added multistage dockerfile to run tests in actions and deploy to pypi

---
+++ Dockerfile
@@ -0,0 +1,36 @@
+FROM condaforge/mambaforge:24.3.0-0 as conda
+
+COPY bandit-agents-dev-env-linux-64.lock lib/bandit-agents-dev-env-linux-64.lock
+
+COPY build_env.sh /lib/build_env.sh
+
+RUN chmod +x /lib/build_env.sh
+
+ENV ENV="BUILD"
+
+RUN ./lib/build_env.sh
+
+FROM debian:stable-slim as unit-test-image
+
+COPY --from=conda /env /env
+
+ENV ENV="TEST"
+
+COPY BanditAgents /lib/BanditAgents
+COPY Tests /lib/Tests
+COPY tox.ini /lib/tox.ini
+
+WORKDIR /lib
+
+CMD ["/env/bin/python3", "-m", "tox", "--current-env"]
+
+FROM debian:stable-slim as publish-lib-image
+
+RUN apt-get -y update; apt-get -y install curl
+
+COPY --from=conda /env /env
+
+ARG ENV
+ENV ENV=$ENV
+
+WORKDIR /lib
--- build_env.sh
+++ build_env.sh
@@ -1,10 +1,46 @@
 #!/bin/sh +x
-if [ -e $CONDA_PREFIX_1/envs/bandit_agents_dev_env ]; then
+
+if [ $ENV = "BUILD" ]; then
+  mamba create \
+    --copy -p ./env \
+    --file /lib/bandit-agents-dev-env-linux-64.lock \
+    && conda clean -afy
+
+  find -name '*.a' -delete &&
+    rm -rf /env/conda-meta &&
+    rm -rf /env/include &&
+    rm /env/lib/libpython3.12.so.1.0 &&
+    find -name '__pycache__' -type d -exec rm -rf '{}' '+' &&
+    rm -rf /env/lib/python3.12/site-packages/pip /env/lib/python3.12/idlelib /env/lib/python3.12/ensurepip \
+      /env/lib/libasan.so.5.0.0 \
+      /env/lib/libtsan.so.0.0.0 \
+      /env/lib/liblsan.so.0.0.0 \
+      /env/lib/libubsan.so.1.0.0 \
+      /env/bin/x86_64-conda-linux-gnu-ld \
+      /env/bin/sqlite3 \
+      /env/bin/openssl \
+      /env/share/terminfo &&
+    find /env/lib/python3.12/site-packages/scipy -name 'tests' -type d -exec rm -rf '{}' '+' &&
+    find /env/lib/python3.12/site-packages/numpy -name 'tests' -type d -exec rm -rf '{}' '+' &&
+    find /env/lib/python3.12/site-packages/pandas -name 'tests' -type d -exec rm -rf '{}' '+' &&
+    find /env/lib/python3.12/site-packages -name '*.pyx' -delete &&
+    rm -rf /env/lib/python3.12/site-packages/uvloop/loop.c
+
+else
+  if [ -e $CONDA_PREFIX_1/envs/bandit_agents_dev_env ]; then
     echo erase bandit_agents_dev_env
     rm -r $CONDA_PREFIX_1/envs/bandit_agents_dev_env
-fi
+  fi
 
-echo creating environment
-conda create \
+  echo creating environment
+  conda create \
     --name bandit_agents_dev_env \
     --file bandit-agents-dev-env-linux-64.lock -y
+
+  echo adding dev dependencies
+  conda update \
+    --name bandit_agents_dev_env \
+    --freeze-installed \
+    --no-update-deps \
+    --file bandit-agents-dev-env-linux-64.lock
+fi
