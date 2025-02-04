271247d5b908c9433dc7f5340a07f9ca09253ae3
The-Faust <vincent.martel.11235@outlook.com>
2025-02-03 20:03:37 -0500

    initial commit for this branch

--- compile_env_lock.sh
+++ compile_env_lock.sh
@@ -3,7 +3,10 @@
 hash conda 2>/dev/null || { echo >&2 "I require conda but it's not installed.  Aborting."; exit 1; }
 conda lock --version 2>/dev/null || { echo >&2 "I require conda-lock but it's not installed. Aborting."; exit 1; }
 
-if [ ! -e mb_assistant_env/mb-assistant-linux-64.lock ] || [ $1 ];
+if [ ! -e bandit-agents-dev-env-linux-64.lock ] || 
+  [ ! -e bandit-agents-dev-env-win-64.lock ] || 
+  [ ! -e bandit-agents-dev-env-osx-64.lock ] || 
+  [ $1 ];
 then
   echo Compiling the conda env lock file this could take a while \\n
 
