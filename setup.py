bdd169b606ac59b02fdfea68fa06fd74f7be26c3
The-Faust <vincent.martel.11235@outlook.com>
2025-02-03 21:01:26 -0500

    Pretty sure there is a better way to debug this... but oh well let's try againe

--- setup.py
+++ setup.py
@@ -7,15 +7,11 @@ def read(filename) -> str:
     return open(os.path.join(os.path.dirname(__file__), filename)).read()
 
 
-with open("BanditAgents/requirements.txt") as f:
-    requirements: list[str] = f.read().splitlines()
-
 setup(
     name="Bandit Agents",
     description="Library to solve k-armed bandit problems",
     packages=find_packages(),
     long_description=read("./README.md"),
-    install_requires=requirements,
     setuptools_git_versioning={"enabled": True},
     setup_requires=[
         "flake8",
